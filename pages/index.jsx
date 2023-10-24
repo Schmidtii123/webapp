186db1f0602dcb3cdc29401ea1b5eda7d7148259
karl0905 <kalle44@live.dk>
2023-10-24 12:50:29 +0200

    books.map

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,48 +1,56 @@
 import MarketplacePost from "@/components/explore/MarketplacePost";
+import { useEffect, useState } from "react";
+import { getAllBooks } from "@/firebase/firebase";
+
 export default function Home() {
+  const [books, setBooks] = useState([]);
+  async function getData() {
+    const data = await getAllBooks();
+    setBooks(data);
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(books);
+
   return (
     <>
-      <main className="flex flex-row flex-wrap w-5/6 m-auto gap-y-8 pb-24">
-        <MarketplacePost
-          img="http://books.google.com/books/content?id=GuAUMQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-          title="Atomic Habits"
-          price="399"
-        />
-        <MarketplacePost
-          img="http://books.google.com/books/content?id=GuAUMQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-          title="Atomic Habits"
-          price="399"
-        />
-        <MarketplacePost
-          img="http://books.google.com/books/content?id=GuAUMQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-          title="Atomic Habits"
-          price="399"
-        />
-        <MarketplacePost
-          img="http://books.google.com/books/content?id=GuAUMQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-          title="Atomic Habits"
-          price="399"
-        />
-        <MarketplacePost
-          img="http://books.google.com/books/content?id=GuAUMQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-          title="Atomic Habits"
-          price="399"
-        />
-        <MarketplacePost
-          img="http://books.google.com/books/content?id=GuAUMQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-          title="Atomic Habits"
-          price="399"
-        />
-        <MarketplacePost
-          img="http://books.google.com/books/content?id=GuAUMQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-          title="Atomic Habits"
-          price="399"
-        />
-        <MarketplacePost
-          img="http://books.google.com/books/content?id=GuAUMQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-          title="Atomic Habits"
-          price="399"
-        />
+      <main className="flex flex-col w-full pb-24 pt-4">
+        <div className="fixed top-0 left-0 bg-white w-full">
+          <h1 className="font-bold text-3xl pl-6">Opdag</h1>
+          <input type="text" />
+          <button>Search</button>
+        </div>
+        <div className="flex flex-wrap justify-around gap-y-4">
+          {books.map((book) => (
+            <MarketplacePost
+              img={book.image}
+              title={book.name}
+              price={book.price}
+            />
+          ))}
+          {books.map((book) => (
+            <MarketplacePost
+              img={book.image}
+              title={book.name}
+              price={book.price}
+            />
+          ))}
+          {books.map((book) => (
+            <MarketplacePost
+              img={book.image}
+              title={book.name}
+              price={book.price}
+            />
+          ))}
+          {books.map((book) => (
+            <MarketplacePost
+              img={book.image}
+              title={book.name}
+              price={book.price}
+            />
+          ))}
+        </div>
       </main>
     </>
   );
