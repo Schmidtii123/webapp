3e724d2127cf0687ad13d4c769bf18e18f332f02
Simon Calundan <simoncalundan@MacBook-Pro-tilhrende-Simon-4.local>
2023-10-24 11:26:15 +0200

    firebase books setup

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useRouter } from "next/router";
+import { getFirestore } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBSTT34mXJ0y79agjqsk0B9rs9lshHuyk8",
@@ -13,6 +22,12 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+/* 
+
+Auth 
+
+*/
 export const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
@@ -28,3 +43,117 @@ export const signInWithGoogle = () => {
       alert(error);
     });
 };
+
+/* 
+
+Firestore (Database)
+
+*/
+
+const db = getFirestore(app);
+
+// Read all books from the database
+export async function getAllBooks() {
+  try {
+    const data = await getDocs(collection(db, "books"));
+    const books = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    return books;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Read books from the database by id
+export async function getBookByID(id) {
+  if (typeof id !== "string") {
+    console.log("Invalid ID, must be of type string");
+    return null;
+  }
+  try {
+    const docRef = doc(db, "books", id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const book = docSnap.data();
+      return book;
+    } else if (!docSnap.exists()) {
+      console.log("Book not found");
+      return null;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Delete book from the database
+export async function deleteBookByID(id) {
+  if (typeof id !== "string") {
+    console.log("Invalid ID, must be of type string");
+    return null;
+  }
+  try {
+    const docRef = doc(db, "books", id);
+    await deleteDoc(docRef);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Validate book input data
+function validateBookInfo(bookInfo) {
+  // Iterates through the bookInfo object and checks each value
+  for (const key in bookInfo) {
+    switch (key) {
+      case "link":
+      case "sellerID":
+      case "name":
+      case "major":
+      case "image":
+        if (!bookInfo[key] || bookInfo[key].trim() === "") {
+          throw new Error(`'${key}' field in bookInfo is empty`);
+        }
+        break;
+      case "semester":
+      case "condition":
+      case "price":
+        if (typeof bookInfo[key] !== "number" || isNaN(bookInfo[key])) {
+          throw new Error(`'${key}' must be a number`);
+        }
+        break;
+      default:
+        throw new Error(`Unknown field: ${key}`);
+    }
+  }
+}
+
+// Update book in the database
+export async function updateBookByID(id, bookInfo) {
+  if (typeof id !== "string") {
+    console.log("Invalid ID, must be of type string");
+    return null;
+  }
+
+  try {
+    const docRef = doc(db, "books", id);
+    const newBook = bookInfo;
+    await updateDoc(docRef, newBook);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Add book to the database
+export async function addBook(bookInfo) {
+  validateBookInfo(bookInfo);
+  try {
+    const book = bookInfo;
+    await addDoc(collection(db, "books"), book);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/* 
+
+Messaging
+
+*/
